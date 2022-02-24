d8fe0ea6da33901184564e17431eb211d3f787f8
guanyun-helo <guanyun.helo@gmail.com>
2022-02-24 11:36:05 +0800

    feat:data fetch duration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteRadar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),    
+    ViteRadar({
+    // Google Analytics tag injection
+    analytics: {
+      id: 'G-PDL744ZCZS',
+    },
+  })],
 })
